634b3ea4975263548ae43565a1e06e889dd5fa6d
protihw <69305991+protihw@users.noreply.github.com>
2023-07-12 10:16:53 -0300

    Update Player.cs

--- Space/Assets/Scripts/Player.cs
+++ Space/Assets/Scripts/Player.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{   
+    // rigidBody
+    private Rigidbody2D rig;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
