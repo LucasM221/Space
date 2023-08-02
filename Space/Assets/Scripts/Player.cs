10b229501088badcf3b3f1937b8c6d338ac41d6a
protihw <69305991+protihw@users.noreply.github.com>
2023-08-02 08:42:58 -0300

    Update Player.cs

--- Space/Assets/Scripts/Player.cs
+++ Space/Assets/Scripts/Player.cs
@@ -6,11 +6,16 @@ public class Player : MonoBehaviour
 {   
     // rigidBody
     private Rigidbody2D rig;
-    
+    // Posição inicial do player
+    private Vector3 posInicial;
+    // Velocidade
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        posInicial = new Vector3(0, -2.3, 0);
+        transform.position = posInicial;
     }
 
     // Update is called once per frame
